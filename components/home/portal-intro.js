a98b3f1ec18a287c261fe2dbd14c4a14cd53d6ec
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-17 06:16:26 +0900

    페이지 틀 복사

--- components/home/portal-intro.js
+++ components/home/portal-intro.js
@@ -1,11 +1,11 @@
 import PortalIntroAnimation from './portal-intro-animation'
 
-export default function PortalIntro(){
+export default function SupportIntro(){
     return (
         <>
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-green-900">
-              모두가 함께합니다. <br></br>
+            모두가 함께합니다. <br></br>
               <br className="hidden lg:inline-block"/>
             </h1>
             <pre className="mb-8 leading-relaxed text-white-900">
@@ -14,8 +14,9 @@ export default function PortalIntro(){
               서로 존중하는 관계속에서 우리 스스로의 가치를 찾아 냅니다. <br/>
             </pre>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                포탈 구경하기</button>
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              포탈 구경하기
+            </button>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
@@ -24,3 +25,4 @@ export default function PortalIntro(){
         </>
     );
 }
+
--- components/home/support-intro.js
+++ components/home/support-intro.js
@@ -4,7 +4,8 @@ export default function SupportIntro(){
     return (
         <>
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-green-900">항상 곁에 있습니다. <br></br>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-green-900">
+            항상 곁에 있습니다. <br></br>
               <br className="hidden lg:inline-block"/>
             </h1>
             <pre className="mb-8 leading-relaxed text-white-900">
@@ -15,7 +16,7 @@ export default function SupportIntro(){
             </pre>
             <div className="flex justify-center">
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                콜센터 연결하기
+              콜센터 연결하기
             </button>
             </div>
           </div>
