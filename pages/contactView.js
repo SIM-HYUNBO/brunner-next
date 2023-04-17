6c8542301e499b63565765fec646f2e2b38d6265
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-17 23:44:09 +0900

    contactView button

--- pages/contactView.js
+++ pages/contactView.js
@@ -89,7 +89,7 @@ export default function IntroView() {
                 </div>
               </div>
             </div>
-            <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">지원하기</button>
           </div>
         </section>
         </div>
