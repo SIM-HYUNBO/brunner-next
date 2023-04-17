5ca17590f53f0899c9c0f109275be87413b4923c
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-17 22:56:18 +0900

    intro 버튼 클릭 구현

--- components/home/service-intro.js
+++ components/home/service-intro.js
@@ -15,7 +15,7 @@ export default function ServiceIntro(){
               바로 시작하십시요.
             </pre>
             <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button onClick={() => router.push('/serviceView')} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 서비스 보기
             </button>
             </div>
