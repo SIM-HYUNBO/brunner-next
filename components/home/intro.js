7a6f32b4d2007804b751b688b8a2df17adb0d420
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-17 23:02:47 +0900

    router 추가

--- components/home/intro.js
+++ components/home/intro.js
@@ -1,6 +1,9 @@
 import IntroAnimation from './intro-animation'
+import { useRouter } from 'next/router'
 
 export default function Intro(){
+  const router = useRouter()
+  
     return (
         <>
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
