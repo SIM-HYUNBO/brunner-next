0e601a72348c016f1038fb0fa9770d2d7e997de2
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-17 23:40:21 +0900

    contactView demo

--- pages/contactView.js
+++ pages/contactView.js
@@ -15,7 +15,7 @@ export default function IntroView() {
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">교육센터 조직도</h1>
               <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
             </div>
             <div className="flex flex-wrap -m-4">
