24c7ccad521f8509a06176c42fa794d3eaba24c9
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-22 09:05:50 +0900

    포털 배포

--- components/board-item.js
+++ components/board-item.js
@@ -1,7 +1,19 @@
 export default function BoardItem({data}){
+    const startDate=data.properties.게시일.date.start;
+    const writer=data.properties.게시자.rich_text[0].plain_text;
+    const content=data.properties.내용;
+    const category=data.properties.분류;
+    const title=data.properties.제목.title[0].plain_text;
+    const tag=data.properties.태그;
+
     return (
         <div className="p-6 m-3 bg-slate-400 rounded-md">
-            <h1>{data.properties.제목.title[0].plain_text} : {data.properties.게시일.date.start}</h1>
+            <h1>{startDate} : </h1>
+            <h1>{writer} : </h1>
+            <h1>{content} : </h1>
+            <h1>{category} : </h1>
+            <h1>{title} : </h1>
+            <h1>{tag} : </h1>
         </div>
     )
 }
