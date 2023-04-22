760714268bd1250eaa2e3828fcda721510f3e97f
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-22 09:45:20 +0900

    컨택배포

--- pages/contactView.js
+++ pages/contactView.js
@@ -16,7 +16,7 @@ export default function ContactView() {
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">교육센터 조직도</h1>
-              <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+              <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Brunner와 함께하는 것은 교육센터를 통해서만 가능합니다. 별도의 입사지원 없이 교육센터에서 교육과정을 이수한 인재를 선발하여 적합한 업무를 배정합니다. 누구나 언제든 도전할 수 있습니다.</p>
             </div>
             <div className="flex flex-wrap -m-4">
               <div className="xl:w-1/3 md:w-1/2 p-4">
