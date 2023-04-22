7f5814b25e97d935a72c08e89640931a67de34da
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-22 09:53:02 +0900

    컨택배포

--- components/home/contact-intro.js
+++ components/home/contact-intro.js
@@ -13,7 +13,7 @@ export default function ContactIntro(){
             <pre className="mb-8 leading-relaxed text-white-900">
               Brunner의 교육센터를 통해 Brunner와 함께 할 수 있습니다. <br/>
               최선을 다해서 구성원들의 미래을 열어드리겠습니다. <br/>
-              지금 바로 연락하십시요.
+              지금 바로 참여하십시요.
             </pre>
             <div className="flex justify-center">
               <button  onClick={() => router.push('/contactView')} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
