1cc30d0bcdc79882410cca32f3c26480676096e1
Hyunbo Sim <hbsim0605@gmail.com>
2023-04-22 09:32:58 +0900

    포털게시

--- components/board-item.js
+++ components/board-item.js
@@ -1,19 +1,24 @@
 export default function BoardItem({data}){
     const startDate=data.properties.게시일.date.start;
     const writer=data.properties.게시자.rich_text[0].plain_text;
-    const content=data.properties.내용;
-    const category=data.properties.분류;
+    const content=data.properties.내용.rich_text[0].plain_text;;
+    const category=data.properties.분류.rich_text[0].plain_text;;
     const title=data.properties.제목.title[0].plain_text;
-    const tag=data.properties.태그;
+    const tags=data.properties.태그.multi_select.map(tagItem=>(tagItem.name));
 
     return (
         <div className="p-6 m-3 bg-slate-700 rounded-md">
-            <h1>{startDate} : </h1>
-            <h1>{writer} : </h1>
-            <h1>{content} : </h1>
-            <h1>{category} : </h1>
-            <h1>{title} : </h1>
-            <h1>{tag} : </h1>
+            <h1>{title}</h1>
+            <hr></hr>
+            <h1>{content}</h1>
+            <hr></hr>
+            <h1>분류: {category}</h1>
+            <hr></hr>
+            <h1>영역:[{tags.join(',')}]</h1>
+            <hr></hr>
+            <h1>게시자: {writer}</h1>
+            <hr></hr>
+            <h1>게시일: {startDate.substring(0, 10)}</h1>
         </div>
     )
 }
