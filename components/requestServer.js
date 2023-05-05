da87d6f234b46e141233889d94dc11796d96e81d
Hyunbo Sim <hbsim0605@gmail.com>
2023-05-05 22:54:19 +0900

    node server 프로젝트 분리

--- components/requestServer.js
+++ components/requestServer.js
@@ -2,7 +2,9 @@ import { NextResponse } from 'next/server';
 
 export default async function RequestServer(jRequest) {
   
-  const res = await fetch(`http://localhost:8080/executeJson/${jRequest}`, {
+  const serverIp='112.156.201.62'; 
+  const serverPort=8080;
+  const res = await fetch(`http://${serverIp}:${serverPort}/executeJson/${jRequest}`, {
     headers: {
       'Content-Type': 'application/json',
     //   'API-Key': process.env.DATA_API_KEY,
