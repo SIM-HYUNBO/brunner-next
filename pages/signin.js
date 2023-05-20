d2b592a219868c01650d16d5ef28765723f40a79
Hyunbo Sim <hbsim0605@gmail.com>
2023-05-21 04:37:37 +0900

    로그인 화면 디자인 변경

--- pages/signin.js
+++ pages/signin.js
@@ -33,8 +33,33 @@ export default function Signin() {
    };
 
   return (
+
     <>
-      <main className={cx(signinCss["form-signin"],"text-center","mt-5")}>
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+        <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <h1 class="title-font font-medium text-3xl text-gray-900">Log in to the Brunner system... </h1>
+          <p class="leading-relaxed mt-4">Enter your ID and password.</p>
+        </div>
+        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign In</h2>
+          <div class="relative mb-4">
+            <label for="id" class="leading-7 text-sm text-gray-600">ID</label>
+            <input type="text" id="id" name="Id" onChange={(e) => changeUserId(e)} class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+          </div>
+          <div class="relative mb-4">
+            <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
+            <input type="password" id="password" name="password" 
+                   class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                   onChange={(e) => changePassword(e)}></input>
+          </div>
+          <button onClick={()=>requestLoginResult()} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
+          <p class="text-xs text-gray-500 mt-3">Nice to meet you.</p>
+        </div>
+      </div>
+    </section>  
+
+      {/* <main className={cx(signinCss["form-signin"],"text-center","mt-5")}>
         <form>
           <h1 className="leading-relaxed mb-8">Brunner에 로그인 합니다.</h1>
 
@@ -57,7 +82,7 @@ export default function Signin() {
                   onClick={()=>requestLoginResult()}>Sign in
           </button>
         </form>
-      </main>
+      </main> */}
     </>
   )
 }
