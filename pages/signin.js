bc523f1615e436f79bf8b468043b8bef0301f1cb
Hyunbo Sim <hbsim0605@gmail.com>
2023-05-21 04:38:29 +0900

    로그인 화면 다시만듬

--- pages/signin.js
+++ pages/signin.js
@@ -58,31 +58,6 @@ export default function Signin() {
         </div>
       </div>
     </section>  
-
-      {/* <main className={cx(signinCss["form-signin"],"text-center","mt-5")}>
-        <form>
-          <h1 className="leading-relaxed mb-8">Brunner에 로그인 합니다.</h1>
-
-          <div className="form-floating">
-            <input type="text" className="form-control text-center" id="floatingInput" placeholder="brunner ID"  onChange={(e) => changeUserId(e)}/>
-            <label htmlFor="leading-relaxed mb-8">ID를 입력하세요.</label>
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control text-center" id="floatingPassword" placeholder="Password"  onChange={(e) => changePassword(e)}/>
-            <label htmlFor="leading-relaxed mb-8">Password를 입력하세요.</label>
-          </div>
-
-          <div className={cx(signinCss.checkbox,"mb-3")}>
-            <label>
-              <input type="checkbox" value="remember-me" /> Remember me
-            </label>
-          </div>
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg m-2" 
-                  type="button"
-                  onClick={()=>requestLoginResult()}>Sign in
-          </button>
-        </form>
-      </main> */}
     </>
   )
 }
