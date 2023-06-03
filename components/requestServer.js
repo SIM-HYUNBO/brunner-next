5af511b82b40433dae8dd87c60063752bfe2baa4
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-03 16:47:50 +0900

    dotenv config

--- components/requestServer.js
+++ components/requestServer.js
@@ -1,5 +1,8 @@
 'use strict'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 async function RequestServerGet(serverIp, serverPort, strJsonRequest) {
   const res = await fetch(`https://${serverIp}:${serverPort}/executeJson/${strJsonRequest}`, {
     headers: {
