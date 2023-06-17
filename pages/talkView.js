314aa80f7fd055a309d427f438faddc8b391c802
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-17 22:11:35 +0900

    talkitem layout

--- pages/talkView.js
+++ pages/talkView.js
@@ -25,6 +25,7 @@ export default function TalkView(pages) {
                 <nav className="flex flex-wrap items-center text-base justify-center">
                 <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">회의</Link>
                   <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">스터디</Link>
+                  <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">IT</Link>
                   <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">모임</Link>
                   <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">연애</Link>
                   <Link className="mr-5 text-gray-600 dark:text-yellow-100 hover:text-gray-400" href="">기타</Link>
