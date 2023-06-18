1fb1aad86a91fa953863049624f0ca9c594ac059
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-18 09:50:53 +0900

    Talk 메뉴 맨앞으로

--- components/header.js
+++ components/header.js
@@ -18,10 +18,10 @@ export default function Header(){
                 </Link>
                 <span className="ml-3 text-xl"></span>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/talk">Talk</Link>
                 <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/">Home</Link>
                 <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/services">Service</Link>
                 <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/support">Support</Link>
-                <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/talk">Talk</Link>
                 <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/contact">Contact</Link>
                 </nav>
                 <DarkModeToggleButton />
