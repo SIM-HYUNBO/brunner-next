372670a3a553a0980d4b9a05ea10c3338cb42b1e
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-24 23:32:52 +0900

    mobile test

--- components/user-info.js
+++ components/user-info.js
@@ -11,9 +11,9 @@ export default function UserInfo(){
     // userInfo : 현재값 가져오기 getter
     // setUserInfo : 현재값 바꾸기 setter
     return (
-        <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400">
+        <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400 align-middle">
          <p >
-            `{userName()}님`
+            {userName()}님
           </p>
           <SignoutButton />
         </div>
