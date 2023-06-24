f0be5b38bb33373acbac53efe12f5658f1e0293d
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-24 23:17:20 +0900

    mobile test

--- components/user-info.js
+++ components/user-info.js
@@ -2,14 +2,21 @@ import SignoutButton from "./signout-button";
 
 export default function UserInfo(){
 
+  const userName = ()=>{
+    return typeof process.env.userInfo == "undefined" || 
+    typeof process.env.userInfo.USER_NAME == "undefined" ? 
+    '': process.env.userInfo.USER_NAME;
+  }
+
     // userInfo : 현재값 가져오기 getter
     // setUserInfo : 현재값 바꾸기 setter
     return (
         <>
-        {process.env.userInfo.USER_NAME &&
-          <p className="ml-3 mr-1 text-gray-600 dark:text-gray-400">
-            {`${process.env.userInfo.USER_NAME}님`}
-          </p>}
+        {
+         <p className="ml-3 mr-1 text-gray-600 dark:text-gray-400">
+            `{userName()}님`
+          </p>
+        }
           <SignoutButton />
         </>
     );
