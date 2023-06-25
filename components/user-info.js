61410f0aa95ad4c9b6ba75ea47ac5eebf66434cd
Hyunbo Sim <hbsim0605@gmail.com>
2023-06-25 23:39:03 +0900

    react strict mode off for Draft editor emoji colorpicker

--- components/user-info.js
+++ components/user-info.js
@@ -14,7 +14,7 @@ export default function UserInfo(){
     return (
         <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400 align-middle">
           <DarkModeToggleButton />
-          <p >
+          <p className="mr-1 ml-1">
             {userName()}
           </p>
           <SignoutButton />
