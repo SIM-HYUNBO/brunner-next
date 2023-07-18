94c79ee5b30e565a085d1f8f665a5fe64cfdb65a
Hyunbo Sim <hbsim0605@gmail.com>
2023-07-18 13:49:28 +0900

    add exception handling while fetch server

--- components/requestServer.js
+++ components/requestServer.js
@@ -3,38 +3,61 @@
 /*
  call backend server
 */
-export default async function RequestServer(method, jRequest){
-  const protocol= process.env.NEXT_PUBLIC_BACKEND_SERVER_PROTOCOL;
-  const serverIp= process.env.NEXT_PUBLIC_BACKEND_SERVER_IP;
-  const serverPort= process.env.NEXT_PUBLIC_BACKEND_SERVER_PORT;
+export default async function RequestServer(method, jRequest) {
+  const protocol = process.env.NEXT_PUBLIC_BACKEND_SERVER_PROTOCOL;
+  const serverIp = process.env.NEXT_PUBLIC_BACKEND_SERVER_IP;
+  const serverPort = process.env.NEXT_PUBLIC_BACKEND_SERVER_PORT;
 
-  if(method === 'GET'){
+  if (method === 'GET') {
     return await RequestServerGet(protocol, serverIp, serverPort, jRequest);
-  } else if (method === 'POST'){ 
+  } else if (method === 'POST') {
     return await RequestServerPost(protocol, serverIp, serverPort, jRequest);
   }
 }
 
 async function RequestServerGet(protocol, serverIp, serverPort, strJsonRequest) {
-  const res = await fetch(`${protocol}://${serverIp}:${serverPort}/executeJson/${strJsonRequest}`, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+  let res = null;
+  let jResponse = null
 
-  const jResponse = res.json();
-  return jResponse;
+  try {
+    res = await fetch(`${protocol}://${serverIp}:${serverPort}/executeJson/${strJsonRequest}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    jResponse = res.json();
+    return jResponse;
+  }
+  catch (e) {
+    jResponse = {};
+    jResponse.error_code = -1;
+    jResponse.error_message = `cannot connect to seerver. ${e}`;
+    return jResponse;
+
+  }
 }
 
 async function RequestServerPost(protocol, serverIp, serverPort, strJsonRequest) {
-  const res = await fetch(`${protocol}://${serverIp}:${serverPort}/executeJson/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: strJsonRequest
-  });
-  
-  const jResponse = res.json();
-  return jResponse;
+  let res = null;
+  let jResponse = null;
+
+  try {
+    res = await fetch(`${protocol}://${serverIp}:${serverPort}/executeJson/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: strJsonRequest
+    });
+
+    jResponse = res.json();
+    return jResponse;
+  }
+  catch (e) {
+    jResponse = {};
+    jResponse.error_code = -1;
+    jResponse.error_message = `cannot connect to seerver. ${e}`;
+    return jResponse;
+  }
 }
