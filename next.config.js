594ec351d81adb4fc0d805d463c05196bea95b2a
Hyunbo Sim <hbsim0605@gmail.com>
2024-05-18 14:58:32 +0900

    next config js for dns

--- next.config.js
+++ next.config.js
@@ -8,19 +8,17 @@ const nextConfig = {
       's3.us-west-2.amazononaws.com'
     ],
   },
-  resolve: {
-    fallback: {
-      // 브라우저에서 'dns' 모듈이 필요할 때 대체할 모듈 설정
-      dns: false  // 브라우저에서 'dns' 모듈을 사용하지 않음
-    }
-  },
   webpack: function (config, options) {
     // console.log(options.webpack.version); // Should be webpack v5 now
     config.experiments = {
       topLevelAwait: true,
       layers: true,
     };
-    config.resolve.fallback = { fs: false };
+    if (!options.isServer)
+      config.resolve.fallback = { fs: false, dns: false };
+    else
+      config.resolve.fallback = { fs: false };
+
     return config;
   }
 }
