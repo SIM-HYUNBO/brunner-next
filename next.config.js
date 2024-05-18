d5248c66d582c580063b8adafaf22fa823dfedfb
Hyunbo Sim <hbsim0605@gmail.com>
2024-05-18 14:51:48 +0900

    dns  제외설정

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
       'www.notion.so',
       'images.unsplash.com',
       's3.us-west-2.amazononaws.com'
-    ]
+    ],
+  },
+  resolve: {
+    fallback: {
+      // 브라우저에서 'dns' 모듈이 필요할 때 대체할 모듈 설정
+      dns: false  // 브라우저에서 'dns' 모듈을 사용하지 않음
+    }
   },
   webpack: function (config, options) {
     // console.log(options.webpack.version); // Should be webpack v5 now
