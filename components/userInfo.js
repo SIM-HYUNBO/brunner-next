1ca6f99ed417f0d594ebc8cd50b87b9836ce22b1
Hyunbo Sim <hbsim0605@gmail.com>
2024-06-23 01:32:15 +0900

    undefined json 변환처리

--- components/userInfo.js
+++ components/userInfo.js
@@ -12,10 +12,12 @@ export default function UserInfo() {
     <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400 align-middle">
       <DarkModeToggleButton />
       <div className="mr-1 ml-1 align-middle">
-        {!(typeof window !== 'undefined' &&
+        {!((typeof window !== 'undefined' &&
           localStorage.getItem('userInfo') !== 'undefined') ? JSON.parse(localStorage.getItem('userInfo')) :
-          null?.userId ? '' :
-            (typeof window !== 'undefined' && JSON.parse(localStorage.getItem('userInfo') !== 'undefined') ? JSON.parse(localStorage.getItem('userInfo')) : null)?.userId + '님'}
+          null)?.userId ? '' :
+          ((typeof window !== 'undefined' &&
+            localStorage.getItem('userInfo') !== 'undefined') ? JSON.parse(localStorage.getItem('userInfo')) :
+            null)?.userId + '님'}
       </div>
       <SignoutButton />
     </div>
