ac8c4dbf541cd7ac98fa52ee63ebefcc3d928e8b
Hyunbo Sim <hbsim0605@gmail.com>
2024-06-22 21:23:38 +0900

    images 삭제

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'www.notion.so',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'images.unsplash.com',
-  //     },
-  //     {
-  //       protocol: 'https',
-  //       hostname: 's3.us-west-2.amazononaws.com',
-  //     }
-  //   ],
-},
+  images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'www.notion.so',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'images.unsplash.com',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 's3.us-west-2.amazononaws.com',
+    //   }
+    // ],
+  },
   webpack: function (config, options) {
     // console.log(options.webpack.version); // Should be webpack v5 now
     config.experiments = {
       topLevelAwait: true,
       layers: true,
     };
-if (!options.isServer) {
-  config.resolve.fallback = { fs: false, dns: false };
-}
-return config;
+    if (!options.isServer) {
+      config.resolve.fallback = { fs: false, dns: false };
+    }
+    return config;
   }
 }
 
