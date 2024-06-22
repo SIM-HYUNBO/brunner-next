515b5f2864d0e557a8b69d884a77255a70436c45
Hyunbo Sim <hbsim0605@gmail.com>
2024-06-22 21:25:46 +0900

    원복

--- next.config.js
+++ next.config.js
@@ -2,20 +2,20 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'www.notion.so',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'images.unsplash.com',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 's3.us-west-2.amazononaws.com',
-    //   }
-    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazononaws.com',
+      }
+    ],
   },
   webpack: function (config, options) {
     // console.log(options.webpack.version); // Should be webpack v5 now
