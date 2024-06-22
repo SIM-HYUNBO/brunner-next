38ac964b03824d178e0b9ba4c92eea8ff11d73a7
Hyunbo Sim <hbsim0605@gmail.com>
2024-06-23 01:43:11 +0900

    로그인사용자 표시 및 로그아웃 버튼 표시

--- components/userInfo.js
+++ components/userInfo.js
@@ -12,8 +12,8 @@ export default function UserInfo() {
     <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400 align-middle">
       <DarkModeToggleButton />
       <div className="mr-1 ml-1 align-middle">
-        {!((typeof window !== 'undefined' &&
-          localStorage.getItem('userInfo') !== 'undefined') ? JSON.parse(localStorage.getItem('userInfo')) :
+        {!((typeof window !== 'undefined' && localStorage.getItem('userInfo') !== 'undefined') ?
+          JSON.parse(localStorage.getItem('userInfo')) :
           null)?.userId ? '' :
           ((typeof window !== 'undefined' &&
             localStorage.getItem('userInfo') !== 'undefined') ? JSON.parse(localStorage.getItem('userInfo')) :
