e3995cc637cee683ceae769dd929954370a20585
Hyunbo Sim <hbsim0605@gmail.com>
2024-06-27 22:48:49 +0900

    비밀번호 입력후 엔터 => 로그인 처리

--- pages/mainPages/signin.js
+++ pages/mainPages/signin.js
@@ -1,59 +1,64 @@
-`use strict`
-
 import Layout from './../../components/layout'
 import Head from 'next/head'
 import BodySection from '../../components/bodySection'
-
-import requestServer from './../../components/requestServer'
 import { useRouter } from 'next/router'
 import { useState, useEffect, useRef } from 'react'
-
+import requestServer from './../../components/requestServer'
 
 export default function Signin() {
   const router = useRouter();
 
   const [userId, setUserId] = useState('');
-  const changeUserIdValue = (e) => {
-    setUserId(e.target.value);
-  };
-
   const [password, setPassword] = useState('');
-  const changePasswordValue = (e) => {
-    setPassword(e.target.value);
-  };
-
   const userIdRef = useRef();
-
-  const requestSignin = async () => {
-    var jRequest = {};
-    var jResponse = null;
-
-    jRequest.commandName = "security.signin";
-    jRequest.systemCode = process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_CODE;
-    jRequest.userId = userId;
-    jRequest.password = password;
-
-    jResponse = await requestServer('POST', JSON.stringify(jRequest));
-
-    if (jResponse.error_code == 0) {
-      process.env.userInfo = jResponse;
-      router.push('/')
-    } else {
-      alert(JSON.stringify(jResponse.error_message));
-    }
-  };
 
   useEffect(() => {
     userIdRef.current.focus();
   }, []);
 
+  const changeUserIdValue = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const changePasswordValue = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const requestSignin = async () => {
+    try {
+      const jRequest = {
+        commandName: "security.signin",
+        systemCode: process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_CODE,
+        userId: userId,
+        password: password
+      };
+
+      const jResponse = await requestServer('POST', JSON.stringify(jRequest));
+
+      if (jResponse.error_code === 0) {
+        process.env.userInfo = jResponse;
+        router.push('/');
+      } else {
+        alert(JSON.stringify(jResponse.error_message));
+      }
+    } catch (error) {
+      console.error('로그인 중 에러 발생:', error);
+      alert('로그인에 실패했습니다. 나중에 다시 시도해 주세요.');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      requestSignin();
+    }
+  };
+
   return (
     <Layout>
       <Head>
         <title>IT 기술 연구소 - Brunner</title>
-        <meta name="description" content="IT 기술 연구소"></meta>
-        <meta rel="icon" href="brunnerLogo.png"></meta>
-        <link></link>
+        <meta name="description" content="IT 기술 연구소" />
+        <link rel="icon" href="brunnerLogo.png" />
       </Head>
       <BodySection className="text-gray-600 body-font">
         <div className="container px-5 mx-auto flex flex-wrap items-center">
@@ -62,49 +67,52 @@ export default function Signin() {
               로그인
             </h1>
             <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-              <p className="leading-relaxed mt-4  mb-5">Enter your ID and password.</p>
+              <p className="leading-relaxed mt-4 mb-5">아이디와 비밀번호를 입력하세요.</p>
             </div>
             <div className="relative mb-4">
               <label htmlFor="id" className="leading-7 text-sm text-gray-400">
-                ID
+                아이디
               </label>
-              <input type="text"
+              <input
+                type="text"
                 ref={userIdRef}
                 id="id"
                 name="Id"
-                onChange={(e) => changeUserIdValue(e)}
+                onChange={changeUserIdValue}
                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-400"
-                htmlFor="password"
-              >
-                Password</label>
-              <input className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              <label className="leading-7 text-sm text-gray-400" htmlFor="password">
+                비밀번호
+              </label>
+              <input
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 type="password"
                 id="password"
                 name="password"
-                onChange={(e) => changePasswordValue(e)}
-              // okkeypress={(e) => { if (e.key == 'Enter') requestSigninResult() }}
-              ></input>
+                onChange={changePasswordValue}
+                onKeyPress={handleKeyPress} // Enter 키 눌림 처리
+              />
             </div>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              onClick={() => requestSignin()}
+            <button
+              className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              onClick={requestSignin}
             >
-              Signin
+              로그인
             </button>
             <p className="text-xs text-gray-500 mt-10">
-              Did you forget password? Reset now.
+              비밀번호를 잊으셨나요? 지금 초기화하세요.
             </p>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
+            <button
+              className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
               onClick={() => router.push('/mainPages/resetPassword')}
             >
-              Reset Password
+              비밀번호 초기화
             </button>
           </div>
         </div>
       </BodySection>
     </Layout>
-  )
+  );
 }
