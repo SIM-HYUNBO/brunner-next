02833782b16c3fbda1ec74da5692504d3b1565b0
심현보 <hbsim0605@gmail.com>
2024-06-29 10:18:46 +0900

    입력기능 추가

--- pages/api/biz/asset.js
+++ pages/api/biz/asset.js
@@ -12,6 +12,9 @@ export default function executeService(txnId, jRequest) {
             case "asset.getIncomeHistory":
                 jResponse = getIncomeHistory(txnId, jRequest);
                 break;
+            case "asset.addIncome":
+                jResponse = addIncome(txnId, jRequest);
+                break;
             default:
                 break;
         }
@@ -21,6 +24,42 @@ export default function executeService(txnId, jRequest) {
         return jResponse;
     }
 }
+
+const addIncome = async (txnId, jRequest) => {
+    var jResponse = {};
+
+    try {
+        jResponse.commanaName = jRequest.commandName;
+
+        var sql = null
+        sql = serviceSQL.getSQL00('insert_TB_COR_INCOME_HIST', 1);
+        var insert_TB_COR_INCOME_HIST_01 = await database.executeSQL(sql,
+            [
+                jRequest.systemCode,
+                jRequest.userId,
+                jRequest.amount,
+                jRequest.comments
+            ]);
+
+        // logger.info(`RESULT:\n${JSON.stringify(select_TB_COR_USER_MST_01.rows[0])}\n`);
+
+        if(insert_TB_COR_INCOME_HIST_01.rowCount == 1){
+            jResponse.error_code = 0;
+            jResponse.error_message = "success";    
+        }
+        else {
+            jResponse.error_code = -1;
+            jResponse.error_message = "fail";    
+ 
+        }
+    } catch (e) {
+        logger.error(`EXCEPTION:\n${e}`);
+        jResponse.error_code = -3; // exception
+        jResponse.error_message = `EXCEPTION:\n${e}`;
+    } finally {
+        return jResponse;
+    }
+};
 
 const getIncomeHistory = async (txnId, jRequest) => {
     var jResponse = {};
