ae0c82a0a5b77b5402911d42b4a2e22abc1e9cf1
심현보 <hbsim0605@gmail.com>
2024-06-30 16:31:17 +0900

    수직 중앙 정렬

--- components/userInfo.js
+++ components/userInfo.js
@@ -8,9 +8,9 @@ export default function UserInfo() {
   }, []);
 
   return (
-    <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400 align-middle">
+    <div className="flex flex-row ml-3 mr-1 text-gray-600 dark:text-gray-400">
       <DarkModeToggleButton />
-      <div className="mr-1 ml-1 align-middle">
+      <div className="mr-1 ml-1 grid place-items-center">
         {getLoginName()}
       </div>
       <SignoutButton />
