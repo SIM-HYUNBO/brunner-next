26ce0d47642af0aaca7154c7280df1823342cc77
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2024-07-03 10:44:44 +0900

    Update BrunnerMessageBox.js

--- components/BrunnerMessageBox.js
+++ components/BrunnerMessageBox.js
@@ -13,7 +13,7 @@ const BrunnerMessageBox = ({ isOpen, message, onConfirm, onClose }) => {
 
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center relative z-0 bg-black bg-opacity-50">
+            <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-0 bg-black bg-opacity-50">
                 <div className="dark:bg-slate-800 bg-slate-200 rounded-lg p-8 max-w-md w-full">
                     <p className="text-lg text-center mb-4">{message}</p>
                     <div className="flex justify-end">
