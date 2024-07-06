1c01a9499f04939253325af01a02f103885d34db
Hyunbo Sim <hbsim0605@gmail.com>
2024-07-07 01:18:38 +0900

    영문  전환

--- pages/mainPages/content/assetContent.js
+++ pages/mainPages/content/assetContent.js
@@ -369,7 +369,7 @@ export default function AssetContent() {
           onClick={handleRefresh}
           className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mb-3"
         >
-          새로고침
+          Refresh
         </button>
         <div className="overflow-x-auto w-full">
           {loading && (
