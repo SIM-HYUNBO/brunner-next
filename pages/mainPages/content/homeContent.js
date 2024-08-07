c767aed5f0633b0fc3a45bd76d47f0f5c9753ecb
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-08 02:57:48 +0900

    governing message 변경

--- pages/mainPages/content/homeContent.js
+++ pages/mainPages/content/homeContent.js
@@ -14,12 +14,12 @@ export default function HomeContent() {
           Asset management
         </h1>
         <div className="main-governing-text">
-          관리만 잘 해도 쉽게 부자될 수 있습니다.<br />
-          많은 사람들과 소통으로 삶을 더 행복하게 합니다.<br />
+          Get Rich Through Smart Management,
+          Boost Happiness with Connections!
         </div>
         <div className="flex flex-col">
           <div className="dark:text-slate-400 mb-2">
-            Brunner를 더 안전하고 편리하게 이용하세요.
+            Use Brunner Safely and Conveniently.
           </div>
           {(!userInfo.isLogin()) && <div>
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mr-2"
