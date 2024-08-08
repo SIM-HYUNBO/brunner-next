32810954d84f96fa71cd47d1956d40ef936603b1
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-09 04:05:56 +0900

    영문전환

--- pages/mainPages/content/homeContent.js
+++ pages/mainPages/content/homeContent.js
@@ -22,7 +22,7 @@ export default function HomeContent() {
         </div>
         <div className="flex flex-col">
           <div className="dark:text-slate-400 mb-2">
-            Brunner를 더 안전하고 편리하게 이용하세요.
+            Enjoy using Brunner in a safer and more convenient way.
           </div>
           {(!userInfo.isLogin()) && <div>
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mr-2"
