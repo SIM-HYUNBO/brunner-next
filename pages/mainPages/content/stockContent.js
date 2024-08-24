bac47b110e7073938fddaaefced363dfc6d2f304
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-25 01:01:28 +0900

    가격 변동에 따른 레이블 색깔 처리

--- pages/mainPages/content/stockContent.js
+++ pages/mainPages/content/stockContent.js
@@ -60,7 +60,7 @@ const StockContent = () => {
         setCurrentTicker(newVal);
         currentTickerRef.current = newVal;
         process.currentTicker = newVal;
-        updateCurrentPrice(0);
+        // updateCurrentPrice(null, null, null);
     }
 
     // 현재 선택한 주식 종목의 데이터
@@ -100,27 +100,29 @@ const StockContent = () => {
     // 현재 가격
     const [currentPrice, setCurrentPrice] = useState();
     const currentPriceRef = useRef(currentPrice);
-    const updateCurrentPrice = (newValue) => {
+    const updateCurrentPrice = (firstData, lastData, newData) => {
         var textColor = '';
 
-        if (currentPriceRef.current == newValue)
-            textColor = 'slate-400';
-        else if (currentPriceRef.current > newValue)
-            textColor = 'blue-600';
+        if (firstData?.y == newData?.y)
+            textColor = 'text-gray-500';
+        else if (firstData?.y > newData?.y)
+            textColor = 'text-blue-500';
         else
-            textColor = 'red-600';
+            textColor = 'text-red-500';
 
-        currentPriceRef.current = newValue;
-        setCurrentPrice(newValue);
+        currentPriceRef.current = newData?.y;
+        setCurrentPrice(newData?.y);
         setCurrentPriceTextColorRef(textColor);
     };
 
     // 현재가격 표시 색깔
-    const [currentPriceTextColor, setCurrentPriceTextColor] = useState();
-    const currentPriceTextColorRef = useRef(currentPriceTextColor);
+    const currentPriceTextColorRef = useRef(null);
     const setCurrentPriceTextColorRef = (newValue) => {
-        setCurrentPriceTextColor(newValue);
-        currentPriceTextColorRef.current = newValue;
+        currentPriceTextColorRef.current.classList.remove('text-gray-500');
+        currentPriceTextColorRef.current.classList.remove('text-red-500');
+        currentPriceTextColorRef.current.classList.remove('text-blue-500');
+
+        currentPriceTextColorRef.current.classList.add(newValue);
 
     };
 
@@ -856,7 +858,8 @@ const StockContent = () => {
                         }}
                     />
                     <input
-                        className={`text-center text-${currentPriceTextColorRef.current} bg-slate-50 dark:bg-slate-800 border border-slate-400 h-10 w-[30%]`}
+                        ref={currentPriceTextColorRef}
+                        className={`text-center bg-slate-50 dark:bg-slate-800 border border-slate-400 h-10 w-[30%]`}
                         type="text"
                         value={currentPriceRef.current}
                         placeholder="Current Price ..."
