527192f63c2647554cb12769c0e6034cd9ed043c
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-25 17:58:01 +0900

    사이트 단위 광고코드만 적용

--- pages/index.js
+++ pages/index.js
@@ -24,74 +24,7 @@ export default function Home() {
       <>
         <Script
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3879149687745447"
-          crossorigin="anonymous"
-        />
-
-        {/* 광고 단위 생성*/}
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3879149687745447"
-          crossorigin="anonymous">
-        </Script>
-
-        {/* 디스플레이 광고 - 사각형 */}
-        <ins class="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5669803344"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-        <Script>
-          (adsbygoogle = window.adsbygoogle || []).push({ });
-        </Script>
-
-        {/* 디스플레이 광고 - 수평형 */}
-        <ins class="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5876281933"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-        <Script>
-          (adsbygoogle = window.adsbygoogle || []).push({ });
-        </Script>
-
-        {/* 디스플레이 광고 - 수직형 */}
-        <ins class="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="6930914120"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-        <Script>
-          (adsbygoogle = window.adsbygoogle || []).push({ });
-        </Script>
-
-        {/* 인피드 광고 - 상단 이미지 광고 */}
-        <ins class="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-format="fluid"
-          data-ad-layout-key="-6t+ed+2i-1n-4w"
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5426260766"></ins>
-        <Script>
-          (adsbygoogle = window.adsbygoogle || []).push({ });
-        </Script>
-
-        {/* 인아티클 */}
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3879149687745447"
-          crossorigin="anonymous"></Script>
-        <ins class="adsbygoogle"
-          style={{ display: 'block', textAlign: 'center' }}
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="2851234162"></ins>
-        <Script>
-          (adsbygoogle = window.adsbygoogle || []).push({ });
-        </Script>
-
-        {/* 멀티플렉스 */}
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3879149687745447"
-          crossorigin="anonymous"></Script>
+          crossorigin="anonymous" />
       </>
     )
   }
