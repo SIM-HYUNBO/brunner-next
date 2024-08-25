bd979791935dbf8e3310771c98acb3310f86b041
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-25 15:17:29 +0900

    DB 유료화 후 광고설정 복원5

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,54 @@ export default function Home() {
     );
   };
 
+  const GoogleInsScript = () => {
+    return (
+      <>
+        {/* 광고 단위 생성 */}
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="5669803344"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="5876281933"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="6930914120"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-format="fluid"
+          data-ad-layout-key="-6t+ed+2i-1n-4w"
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="5426260766"></ins>
+
+        <ins className="adsbygoogle"
+          style={{ display: 'block', textAlign: 'center' }}
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="2851234162"></ins>
+
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-format="autorelaxed"
+          data-ad-client="ca-pub-3879149687745447"
+          data-ad-slot="7911989152"></ins>
+      </>
+    )
+  }
+
   return (
     <Layout>
       <Head>
@@ -54,6 +102,7 @@ export default function Home() {
         <meta name="google-adsense-account" content="ca-pub-3879149687745447"></meta>
         <GoogleScript />
       </Head>
+      <GoogleInsScript />
       <BodySection>
         <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
           <HomeContent></HomeContent>
