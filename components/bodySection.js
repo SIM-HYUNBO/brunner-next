a10141c5089cba49030d201ea8a62710f750d31d
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-25 15:10:11 +0900

    DB 유료화 후 광고설정 복원4

--- components/bodySection.js
+++ components/bodySection.js
@@ -2,6 +2,49 @@
 export default function BodySection({ children }) {
     return (
         <section className="text-gray-600 body-font h-full">
+            <p>
+                {/* 광고 단위 생성 */}
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="5669803344"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="5876281933"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="6930914120"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-6t+ed+2i-1n-4w"
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="5426260766"></ins>
+
+                <ins className="adsbygoogle"
+                    style={{ display: 'block', textAlign: 'center' }}
+                    data-ad-layout="in-article"
+                    data-ad-format="fluid"
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="2851234162"></ins>
+
+                <ins className="adsbygoogle"
+                    style={{ display: 'block' }}
+                    data-ad-format="autorelaxed"
+                    data-ad-client="ca-pub-3879149687745447"
+                    data-ad-slot="7911989152"></ins>
+            </p>
             {children}
         </section>
     );
--- pages/index.js
+++ pages/index.js
@@ -40,48 +40,6 @@ export default function Home() {
             }
           }}
         />
-
-        {/* 광고 단위 생성 */}
-        <ins className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5669803344"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-
-        <ins className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5876281933"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-
-        <ins className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="6930914120"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-
-        <ins className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-format="fluid"
-          data-ad-layout-key="-6t+ed+2i-1n-4w"
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5426260766"></ins>
-
-        <ins className="adsbygoogle"
-          style={{ display: 'block', textAlign: 'center' }}
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="2851234162"></ins>
-
-        <ins className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-format="autorelaxed"
-          data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="7911989152"></ins>
       </>
     );
   };
@@ -90,9 +48,9 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Brunner Home</title>
-        {/* <meta name="description" content="Stock Charts and Market Analysis, Asset Management"></meta>
+        <meta name="description" content="Stock Charts and Market Analysis, Asset Management"></meta>
         <meta name="keywords" content="Stock, Chart, Asset, Investment" />
-        <meta rel="icon" href="/brunnerLogo.png"></meta> */}
+        <meta rel="icon" href="/brunnerLogo.png"></meta>
         <meta name="google-adsense-account" content="ca-pub-3879149687745447"></meta>
         <GoogleScript />
       </Head>
