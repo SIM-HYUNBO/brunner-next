3a8d0519fe233f2c428e13aa772ca08f1873727e
Hyunbo Sim <hbsim0605@gmail.com>
2024-08-25 12:50:34 +0900

    재시도2

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default function Home() {
 
   }, []);
 
-  const GoogleScript = () => {
+  const GoogleAds = () => {
     useEffect(() => {
-      // Google Ads 스크립트가 로드된 후 초기화
+      // Google Ads 스크립트 로드 후 광고 초기화
       if (window.adsbygoogle) {
         (window.adsbygoogle = window.adsbygoogle || []).push({});
       }
@@ -34,7 +34,7 @@ export default function Home() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3879149687745447"
           strategy="afterInteractive"
           onLoad={() => {
-            // 스크립트 로드 후 adsbygoogle 초기화
+            // 스크립트 로드 후 광고 초기화
             if (window.adsbygoogle) {
               (window.adsbygoogle = window.adsbygoogle || []).push({});
             }
@@ -47,41 +47,47 @@ export default function Home() {
           data-ad-client="ca-pub-3879149687745447"
           data-ad-slot="5669803344"
           data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
+          data-full-width-responsive="true"
+        ></ins>
 
-        {/* <ins className="adsbygoogle"
+        <ins className="adsbygoogle"
           style={{ display: 'block' }}
           data-ad-client="ca-pub-3879149687745447"
           data-ad-slot="5876281933"
           data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
+          data-full-width-responsive="true"
+        ></ins>
 
         <ins className="adsbygoogle"
           style={{ display: 'block' }}
           data-ad-client="ca-pub-3879149687745447"
           data-ad-slot="6930914120"
           data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
+          data-full-width-responsive="true"
+        ></ins>
 
         <ins className="adsbygoogle"
           style={{ display: 'block' }}
           data-ad-format="fluid"
           data-ad-layout-key="-6t+ed+2i-1n-4w"
           data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="5426260766"></ins>
+          data-ad-slot="5426260766"
+        ></ins>
 
         <ins className="adsbygoogle"
           style={{ display: 'block', textAlign: 'center' }}
           data-ad-layout="in-article"
           data-ad-format="fluid"
           data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="2851234162"></ins>
+          data-ad-slot="2851234162"
+        ></ins>
 
         <ins className="adsbygoogle"
           style={{ display: 'block' }}
           data-ad-format="autorelaxed"
           data-ad-client="ca-pub-3879149687745447"
-          data-ad-slot="7911989152"></ins> */}
+          data-ad-slot="7911989152"
+        ></ins>
       </>
     );
   };
