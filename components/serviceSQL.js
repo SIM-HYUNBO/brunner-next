09164adae53acb136ec4a9bd9a77084c988d6fce
Hyunbo Sim <hbsim0605@gmail.com>
2024-09-07 10:36:11 +0900

    SQL 신규생성

--- components/serviceSQL.js
+++ components/serviceSQL.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import * as Constants from '@/components/constants';
 import * as userInfo from '@/components/userInfo';
 import requestServer from '@/components/requestServer';
 import BrunnerMessageBox from '@/components/BrunnerMessageBox';
 
-const AutoResizeTextarea = ({ value, onChange, readOnly }) => {
+const AutoResizeTextarea = ({ name, value, onChange, readOnly }) => {
     const textareaRef = useRef(null);
 
     // 높이 조정 로직
@@ -12,24 +12,30 @@ const AutoResizeTextarea = ({ value, onChange, readOnly }) => {
         const textarea = textareaRef.current;
         if (textarea) {
             textarea.style.height = 'auto'; // 높이 초기화
-            textarea.style.height = `${textarea.scrollHeight}px`; // 내용에 맞게 높이 설정
+            textarea.style.height = `${textarea.scrollHeight + 5}px`; // 내용에 맞게 높이 설정
         }
     }, [value]);
+
+    const handleResizeHeight = useCallback(() => {
+        textareaRef.current.style.height = textareaRef.current.scrollHeight + "px";
+    }, []);
 
     const handleChange = (e) => {
         if (!readOnly) {
             onChange(e); // 값 변경 처리
         }
+        handleResizeHeight()
     };
 
     return (
         <textarea
             ref={textareaRef}
+            name={name}
             value={value}
             onChange={handleChange}
             readOnly={readOnly}
             className={`resize-none w-full border border-gray-300 rounded-md p-2 mt-1 ${readOnly ? 'bg-gray-100' : ''}`}
-            rows="1"
+            rows="10"
         />
     );
 };
@@ -65,6 +71,7 @@ const ServiceSQL = () => {
         });
     };
 
+    const [isEditing, setIsEditing] = useState(false);
     const [queries, setQueries] = useState([]);
     const [form, setForm] = useState({
         system_code: '',
@@ -72,22 +79,26 @@ const ServiceSQL = () => {
         sql_seq: '',
         sql_content: ''
     });
-    const [currentServiceSQL, setCurrentServiceSQL] = useState(null);
+    var [currentServiceSQL, setCurrentServiceSQL] = useState(null);
 
     // Fetch queries from API
     useEffect(() => {
         fetchServiceSQLs();
     }, []);
 
+    var editingServiceSQL;
+
     // Handle form input changes
     const handleInputChange = (e) => {
-        setForm((prevForm) => ({
-            ...prevForm,
-            'system_code': currentServiceSQL.system_code,
-            'sql_name': currentServiceSQL.sql_name,
-            'sql_seq': currentServiceSQL.sql_seq,
-            'sql_content': e.target.value
-        }));
+        const { name, value } = e.target;
+        editingServiceSQL = {
+            'system_code': name === 'system_code' ? value : form.system_code,
+            'sql_name': name === 'sql_name' ? value : form.sql_name,
+            'sql_seq': name === 'sql_seq' ? value : form.sql_seq,
+            'sql_content': name === 'sql_content' ? value : form.sql_content
+        };
+
+        setForm((prevForm) => (editingServiceSQL));
     };
 
     const fetchServiceSQLs = async () => {
@@ -127,11 +138,11 @@ const ServiceSQL = () => {
             console.info(`${form.system_code} ${form.sql_name} ${form.sql_seq} ${form.sql_content}`);
 
             jRequest.commandName = Constants.COMMAND_SERVICESQL_UPDATE_SERVICE_SQL;
-            jRequest.systemCode = currentServiceSQL.system_code;
-            jRequest.sqlName = currentServiceSQL.sql_name;
-            jRequest.sqlSeq = currentServiceSQL.sql_seq;
+            jRequest.systemCode = form.system_code;
+            jRequest.sqlName = form.sql_name;
+            jRequest.sqlSeq = form.sql_seq;
             jRequest.sqlContent = form.sql_content;
-            jRequest.action = currentServiceSQL ? 'Update' : 'Create';
+            jRequest.action = isEditing ? 'Create' : 'Update';
             jRequest.userId = userInfo.getLoginUserId();
 
             setLoading(true); // 데이터 로딩 시작
@@ -148,6 +159,7 @@ const ServiceSQL = () => {
                     sql_content: ''
                 });
                 setCurrentServiceSQL(null);
+                setIsEditing(false);
                 fetchServiceSQLs()
             }
             else
@@ -156,6 +168,23 @@ const ServiceSQL = () => {
         } catch (error) {
             openModal(error);
         }
+    };
+
+    const handleNew = () => {
+        setForm({
+            system_code: '',
+            sql_name: '',
+            sql_seq: '',
+            sql_content: ''
+        });
+        setCurrentServiceSQL({
+            'system_code': '',
+            'sql_name': '',
+            'sql_seq': '',
+            'sql_content': '',
+
+        });
+        setIsEditing(true); // 신규 데이터 생성 모드 활성화
     };
 
     // Handle edit action
@@ -193,6 +222,12 @@ const ServiceSQL = () => {
                 />
 
                 <h1 className="text-xl font-bold mb-4">Service SQL Management</h1>
+                <button
+                    onClick={handleNew}
+                    className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-500 focus:ring-opacity-50 mb-4"
+                >
+                    New SQL
+                </button>
                 <div className="mb-4">
                     <label className="block mb-2">
                         <span className="text-gray-700">System Code:</span>
@@ -200,7 +235,8 @@ const ServiceSQL = () => {
                             type="text"
                             name="system_code"
                             value={form.system_code}
-                            readOnly
+                            onChange={handleInputChange}
+                            readOnly={!isEditing}
                             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
                             required
                         />
@@ -211,7 +247,8 @@ const ServiceSQL = () => {
                             type="text"
                             name="sql_name"
                             value={form.sql_name}
-                            readOnly
+                            onChange={handleInputChange}
+                            readOnly={!isEditing}
                             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
                             required
                         />
@@ -222,7 +259,8 @@ const ServiceSQL = () => {
                             type="text"
                             name="sql_seq"
                             value={form.sql_seq}
-                            readOnly
+                            onChange={handleInputChange}
+                            readOnly={!isEditing}
                             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
                             required
                         />
@@ -230,9 +268,10 @@ const ServiceSQL = () => {
                     <label className="block mb-4">
                         <span className="text-gray-700">SQL Content:</span>
                         <AutoResizeTextarea
+                            name="sql_content"
                             value={form.sql_content}
                             onChange={handleInputChange}
-                            readOnly={currentServiceSQL === null}
+                            readOnly={!isEditing}
                         />
                     </label>
                     <button
@@ -279,7 +318,7 @@ const ServiceSQL = () => {
                                     </button>
                                     <button
                                         onClick={() => handleDelete(query.id)}
-                                        className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 ml-2"
+                                        className="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
                                     >
                                         Delete
                                     </button>
