548b55c21e690e865066f25b2fc5e0b1e8b58ff4
Hyunbo Sim <hbsim0605@gmail.com>
2024-09-09 00:50:55 +0900

    로고 위치 재조정

--- components/header.js
+++ components/header.js
@@ -12,15 +12,15 @@ export default function Header() {
         <>
             <header className="text-gray-600 body-font mb-10">
                 <PageContainer>
+                    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
+                        <Image src="/brunnerLogo.png" height={100} width={100} alt='brunner logo' priority='true' />
+                    </Link>
+                    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
+                        <h1 className="title-font sm:text-4xl text-3xl m-5 font-medium text-orange-900">Brunner
+                            <br className="hidden lg:inline-block" />
+                        </h1>
+                    </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
-                            <Image src="/brunnerLogo.png" height={100} width={100} alt='brunner logo' priority='true' />
-                        </Link>
-                        <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
-                            <h1 className="title-font sm:text-4xl text-3xl m-5 font-medium text-orange-900">Brunner
-                                <br className="hidden lg:inline-block" />
-                            </h1>
-                        </Link>
                         <span className="ml-3 text-xl"></span>
                         <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/mainPages/contact">
                             Contact
