0a07526632a2e24568340ec86af569a384892fe5
Hyunbo Sim <hbsim0605@gmail.com>
2024-09-27 23:08:05 +0900

    오류 재빌드

--- pages/mainPages/signin.js
+++ pages/mainPages/signin.js
@@ -100,70 +100,70 @@ export default function Signin() {
       />
       <Layout>
         <Head>
-          Stock Quotes and Investment Information - Brunner-Next</title>
-        <meta name="description" content="IT 기술 연구소" />
-        <link rel="icon" href="/brunnerLogo.png" />
-      </Head>
-      <BodySection>
-        <DivContainer>
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-10 font-medium text-green-900">
-              Sign in
-            </h1>
-            <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-              <p className="leading-relaxed mt-4 mb-5">Inut ID and Password.</p>
+          <title>Stock Quotes and Investment Information - Brunner-Next</title>
+          <meta name="description" content="IT 기술 연구소" />
+          <link rel="icon" href="/brunnerLogo.png" />
+        </Head>
+        <BodySection>
+          <DivContainer>
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-10 font-medium text-green-900">
+                Sign in
+              </h1>
+              <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+                <p className="leading-relaxed mt-4 mb-5">Inut ID and Password.</p>
+              </div>
+              <div className="relative mb-4">
+                <label htmlFor="id" className="leading-7 text-sm text-gray-400">
+                  ID
+                </label>
+                <input
+                  type="text"
+                  ref={userIdRef}
+                  id="id"
+                  name="Id"
+                  onChange={changeUserIdValue}
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label className="leading-7 text-sm text-gray-400" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={changePasswordValue}
+                  onKeyPress={handleKeyPress} // Enter 키 눌림 처리
+                />
+              </div>
+              <button
+                className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                onClick={requestSignin}>
+                Sign in
+              </button>
+              <p className="text-xs text-gray-500 mt-10">
+                Forgot your password? Reset now.
+              </p>
+              <button
+                className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
+                onClick={() => router.push('/mainPages/resetPassword')}>
+                Reset password
+              </button>
+              <p className="text-xs text-gray-500 mt-10">
+                You can delete account.
+              </p>
+              <button
+                className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
+                onClick={() => router.push('/mainPages/resetPassword')}>
+                Leave & Delete Account
+              </button>
             </div>
-            <div className="relative mb-4">
-              <label htmlFor="id" className="leading-7 text-sm text-gray-400">
-                ID
-              </label>
-              <input
-                type="text"
-                ref={userIdRef}
-                id="id"
-                name="Id"
-                onChange={changeUserIdValue}
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label className="leading-7 text-sm text-gray-400" htmlFor="password">
-                Password
-              </label>
-              <input
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                type="password"
-                id="password"
-                name="password"
-                onChange={changePasswordValue}
-                onKeyPress={handleKeyPress} // Enter 키 눌림 처리
-              />
-            </div>
-            <button
-              className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              onClick={requestSignin}>
-              Sign in
-            </button>
-            <p className="text-xs text-gray-500 mt-10">
-              Forgot your password? Reset now.
-            </p>
-            <button
-              className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
-              onClick={() => router.push('/mainPages/resetPassword')}>
-              Reset password
-            </button>
-            <p className="text-xs text-gray-500 mt-10">
-              You can delete account.
-            </p>
-            <button
-              className="text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-5"
-              onClick={() => router.push('/mainPages/resetPassword')}>
-              Leave & Delete Account
-            </button>
-          </div>
-        </DivContainer>
-      </BodySection>
-    </Layout >
+          </DivContainer>
+        </BodySection>
+      </Layout >
     </>
   );
 }
