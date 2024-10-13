4269b35f105d2007b8b64bb60d8a2dd1375d6884
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-13 18:07:10 +0900

    디자인 변경

--- components/leftMenu.js
+++ components/leftMenu.js
@@ -4,12 +4,12 @@ export default function LeftMenu() {
 
     return (
 
-        <aside className="bg-slate-500 dark:bg-slate-800 dark:text-green-900  px-4 pt-32 desktop:w-32">
+        <aside className=" dark:bg-slate-800 px-4 pt-32 desktop:w-32">
             <nav className="fixed">
                 <ul>
-                    <li><a href="/" className="block py-2">Home</a></li>
-                    <li><a href="/mainPages/clips" className="block py-2">Clips</a></li>
-                    <li><a href="/mainPages/contact" className="block py-2">Contact</a></li>
+                    <li><a href="/" className="block text-green-900 dark:text-green-200 py-2">Home</a></li>
+                    <li><a href="/mainPages/clips" className="block text-green-900 dark:text-green-200 py-2">Clips</a></li>
+                    <li><a href="/mainPages/contact" className="block text-green-900 dark:text-green-200 py-2">Contact</a></li>
                 </ul>
             </nav>
         </aside>
