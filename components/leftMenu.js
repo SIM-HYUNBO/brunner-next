a53bb27a145e6c26e35c745bc65c40a8321d3985
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-14 05:27:43 +0900

    device detect

--- components/leftMenu.js
+++ components/leftMenu.js
@@ -1,24 +1,27 @@
 `use strict`
 import Link from "next/link";
+import { isMobile, isTablet, isBrowser } from 'react-device-detect';
 
 export default function LeftMenu() {
 
     return (
-
-        <aside className=" dark:bg-slate-800 px-2 pt-32 w-32 desktop:px-4 desktop:pt-32 desktop:w-32">
-            <nav className="fixed">
-                <ul>
-                    <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/">
-                        Home
-                    </Link>
-                    <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/mainPages/clips">
-                        Clips
-                    </Link>
-                    <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/mainPages/contact">
-                        Contact
-                    </Link>
-                </ul>
-            </nav>
-        </aside>
+        <>
+            {(!isMobile) && <aside className=" dark:bg-slate-800 px-2 pt-32 w-32 desktop:px-4 desktop:pt-32 desktop:w-32">
+                <nav className="fixed">
+                    <ul>
+                        <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/">
+                            Home
+                        </Link>
+                        <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/mainPages/clips">
+                            Clips
+                        </Link>
+                        <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/mainPages/contact">
+                            Contact
+                        </Link>
+                    </ul>
+                </nav>
+            </aside>
+            }
+        </>
     );
 };
