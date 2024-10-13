768d957f883d8a3c43965934c16d10fc31f10dea
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-14 01:41:02 +0900

    모바일에서 마진 부여

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <>
       <div className="flex bg-primary min-h-screen justify-center">
         <LeftMenu></LeftMenu>
-        <div className="px-2 w-full desktop:w-3/4 mobile:px-3">
+        <div className="px-2 w-full desktop:w-3/4 mobile:ml-5">
           <Header />
           <main>{children}</main>
           <Footer />
