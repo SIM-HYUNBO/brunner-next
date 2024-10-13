0e1f8d4558e85ad63e19d23dff81faba0c8ec92a
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-14 05:27:43 +0900

    device detect

--- components/header.js
+++ components/header.js
@@ -21,6 +21,9 @@ export default function Header() {
                         </h1>
                     </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                        <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/mainPages/clips">
+                            Clips
+                        </Link>
                         <Link className="mr-5 text-gray-600 dark:text-gray-100 hover:text-gray-400" href="/mainPages/contact">
                             Contact
                         </Link>
