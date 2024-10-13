3d0d492e383090e2edf449400b4419f38b3383cb
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-13 21:24:33 +0900

    leftMenu 사이즈

--- components/leftMenu.js
+++ components/leftMenu.js
@@ -5,7 +5,7 @@ export default function LeftMenu() {
 
     return (
 
-        <aside className=" dark:bg-slate-800 desktop:px-4 desktop:pt-32 desktop:w-32">
+        <aside className=" dark:bg-slate-800 px-1 pt-32 w-32 desktop:px-4 desktop:pt-32 desktop:w-32">
             <nav className="fixed">
                 <ul>
                     <Link className="block text-gray-600 dark:text-gray-100 py-2" href="/">
