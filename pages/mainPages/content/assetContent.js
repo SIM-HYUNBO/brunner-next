42f982488b5f1e4a6faf0903536030a4b438607c
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-13 13:33:14 +0900

    전체 다시 빌드

--- pages/mainPages/content/assetContent.js
+++ pages/mainPages/content/assetContent.js
@@ -8,7 +8,7 @@ import * as userInfo from '@/components/userInfo';
 import moment from 'moment';
 import BrunnerMessageBox from '@/components/brunnerMessageBox';
 import * as Constants from '@/components/constants';
-import DivContainer from "@/components/DivContainer"
+import DivContainer from "@/components/divContainer"
 
 export default function AssetContent() {
   const router = useRouter();
