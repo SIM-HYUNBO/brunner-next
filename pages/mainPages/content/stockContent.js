22c7789053b8acfc8baae64f3ff5390cb3021b94
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-20 02:24:31 +0900

    코드 정리

--- pages/mainPages/content/stockContent.js
+++ pages/mainPages/content/stockContent.js
@@ -981,12 +981,11 @@ const StockContent = () => {
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
         </div>
       )}
-
       <SearchPanel />
-
-      {currentTickerStockDataRef.current && renderChart()}
-
-      {UserInfo.isLogin() && <AssetContent></AssetContent>}
+      {currentTickerStockDataRef.current &&
+        renderChart()}
+      {UserInfo.isLogin() &&
+        <AssetContent></AssetContent>}
     </DivContainer>
   );
 };
