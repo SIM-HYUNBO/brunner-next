b610b88158fa23c9e83fc840344b713d1a33ac1e
Hyunbo Sim <hbsim0605@gmail.com>
2024-10-21 00:59:39 +0900

    검색결과 위치이동

--- pages/mainPages/content/stockContent.js
+++ pages/mainPages/content/stockContent.js
@@ -1000,6 +1000,12 @@ const StockContent = () => {
             className="h-8 w-8 ml-1 mt-1 align-middle" // 적절한 크기로 조정
           />
         </div>
+        {currentTickerStockDataRef.current && (
+          <div className="flex flex-col w-full">
+            {renderChart()}
+          </div>
+        )}
+
       </div>
     );
   }
@@ -1018,11 +1024,6 @@ const StockContent = () => {
         </div>
       )}
       <SearchPanel className="w-full" />
-      {currentTickerStockDataRef.current && (
-        <div className="flex flex-col w-full">
-          {renderChart()}
-        </div>
-      )}
       {UserInfo.isLogin() &&
         <AssetContent></AssetContent>}
     </DivContainer>
