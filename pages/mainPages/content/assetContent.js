6bbe9b097e41b98b28cd1f7c7a3831351b907892
심현보 <hbsim0605@gmail.com>
2025-02-23 17:23:18 +0900

    테이블 기반 개발 소스 일반화

--- pages/mainPages/content/assetContent.js
+++ pages/mainPages/content/assetContent.js
@@ -45,22 +45,25 @@ export default function AssetContent() {
 
   const router = useRouter();
 
-  const [tableData, setTableData] = useState([]);
+  // 테이블 데이터 CRUD 일반화
+  useEffect(() => {
+    fetchTableData();
+  }, []);
+  
+  // 테이블 데이터 조회 결과
+  const [tableData, setTableData] = useState([]); 
   const tableDataRef = useRef(tableData); 
   const setTableDataRef = (data) => {
     tableDataRef.current = data;
     setTableData(data);
   };
 
+  // 테이블 데이터 입력 필드
   const [amountInput, setAmountInput] = useState("");
   const [commentInput, setCommentInput] = useState("");
 
   const amountInputRef = useRef(null);  // Amount input ref
   const commentInputRef = useRef(null);  // Comment input ref
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   // 테이블 컬럼 정의
   const columns = React.useMemo(
@@ -122,7 +125,7 @@ export default function AssetContent() {
         Cell: ({ row }) => (
           <div className="flex justify-center">
             <button
-              onClick={() => handleSave(row)}
+              onClick={() => handleUpdateTableData(row)}
               className="p-2 rounded"
               title="Save"
             >
@@ -133,7 +136,7 @@ export default function AssetContent() {
               />
             </button>
             <button
-              onClick={() => handleDelete(row.index)}
+              onClick={() => handleDeleteTableData(row.index)}
               className="p-2 rounded"
               title="Delete"
             >
@@ -161,16 +164,12 @@ export default function AssetContent() {
       useSortBy
   );
 
-  const fetchData = async function () {
-    await fetchIncomeHistory();
-  };
-
-  const fetchIncomeHistory = async () => {
-    const result = await requestGetIncomeHistory();
+  const fetchTableData = async () => {
+    const result = await requestTableData();
     setTableDataRef(result);
   };
 
-  const requestGetIncomeHistory = async () => {
+  const requestTableData = async () => {
     const userId = userInfo.getLoginUserId();
     if (!userId) return [];
 
@@ -199,7 +198,7 @@ export default function AssetContent() {
     }
   };
 
-  const handleAddIncome = async () => {
+  const handleAddNewTableData = async () => {
     const userId = userInfo.getLoginUserId();
     if (!userId) return;
     if (!amountInput) {
@@ -226,7 +225,7 @@ export default function AssetContent() {
 
       if (jResponse.error_code === 0) {
         openModal(constants.messages.MESSAGE_SUCCESS_ADDED);
-        fetchIncomeHistory(); 
+        fetchTableData(); 
         setAmountInput("");
         setCommentInput("");
       } else {
@@ -252,7 +251,7 @@ export default function AssetContent() {
     }
   };
 
-  const handleSave = async (row) => {
+  const handleUpdateTableData = async (row) => {
     const userId = userInfo.getLoginUserId();
     if (!userId) return;
 
@@ -281,10 +280,10 @@ export default function AssetContent() {
       openModal("Successfully updated.");
 
       if (jResponse.error_code === 0) {
-        fetchIncomeHistory();
+        fetchTableData();
       } else {
         openModal(jResponse.error_message);
-        fetchIncomeHistory();
+        fetchTableData();
       }
     } catch (error) {
       setLoading(false); 
@@ -294,10 +293,10 @@ export default function AssetContent() {
   };
 
   const handleRefresh = () => {
-    fetchIncomeHistory(); 
+    fetchTableData(); 
   };
 
-  const handleDelete = async (rowIndex) => {
+  const handleDeleteTableData = async (rowIndex) => {
     const userId = userInfo.getLoginUserId();
     if (!userId) return;
 
@@ -320,7 +319,7 @@ export default function AssetContent() {
 
       if (jResponse.error_code === 0) {
         openModal(constants.messages.MESSAGE_SUCCESS_DELETED);
-        fetchIncomeHistory(); 
+        fetchTableData(); 
       } else {
         openModal(jResponse.error_message);
       }
@@ -337,45 +336,67 @@ export default function AssetContent() {
 
   return (
     <>
-      <DivContainer>
-        <table {...getTableProps()} className="w-full text-left table-auto">
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    {column.render("Header")}
-                    <span>
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? " 🔽"
-                          : " 🔼"
-                        : ""}
-                    </span>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()} className="p-2 border-b dark:border-slate-700">
-                        {cell.render("Cell")}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </DivContainer>
+      {modalContent.isOpen && (
+        <BrunnerMessageBox
+          isOpen={modalContent.isOpen}
+          message={modalContent.message}
+          onConfirm={modalContent.onConfirm}
+          onClose={modalContent.onClose}
+        />
+      )}
 
+      <DivContainer>
+
+      {/* 조회 조건 영역 */}
+      <div className="flex justify-end w-full p-4 bg-gray-100 mt-5">    
+        {/* 조회 버튼 */}
+        <button
+            onClick={handleRefresh}
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mb-3"
+        >
+            Refresh
+        </button>
+      </div>
+
+      {/* 조회 결과 테이블 */}
+      <table {...getTableProps()} className="w-full text-left table-auto">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} className="p-2 border-b dark:border-slate-700">
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {/* 테이블 데이터 입력 패널 */}
       <div className="mb-5 table w-full bg-slate-100 mt-2 p-2">
         <input
           type="text"
@@ -386,8 +407,10 @@ export default function AssetContent() {
           className="mr-3 p-2 border rounded dark:text-gray-300 text-right table-column"
           ref={amountInputRef}  // ref 추가
         />
-        <div className="relative flex-grow">
-          <input
+      </div>
+
+      <div className="relative flex-grow">
+        <input
             type="text"
             name="commentInput"
             value={commentInput}
@@ -397,24 +420,16 @@ export default function AssetContent() {
             style={{ marginLeft: "-2px" }}
             ref={commentInputRef}  // ref 추가
           />
-        </div>
-        <button
-          onClick={handleAddIncome}
+      </div>
+      <button
+          onClick={handleAddNewTableData}
           className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 justify"
           style={{ alignSelf: "flex-end" }}
         >
           Add
-        </button>
-      </div>
+      </button>
 
-      {modalContent.isOpen && (
-        <BrunnerMessageBox
-          isOpen={modalContent.isOpen}
-          message={modalContent.message}
-          onConfirm={modalContent.onConfirm}
-          onClose={modalContent.onClose}
-        />
-      )}
+      </DivContainer>
     </>
   );
 }
