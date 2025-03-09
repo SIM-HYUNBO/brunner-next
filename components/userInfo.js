a0688456a44e35789d7de9b67d8eacd0c78b0e79
심현보 <hbsim0605@gmail.com>
2025-03-09 14:18:41 +0900

    @components 에약경로 지정

--- components/userInfo.js
+++ components/userInfo.js
@@ -1,7 +1,7 @@
 `use strict`
 
-import SignoutButton from "./signoutButton";
-import DarkModeToggleButton from "./darkModeToggleButton";
+import SignoutButton from "@components/signoutButton";
+import DarkModeToggleButton from "@components/darkModeToggleButton";
 import { useState, useEffect } from 'react';
 
 export default function UserInfo() {
