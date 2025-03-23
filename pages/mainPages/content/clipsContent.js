1318a05728e1c4e728a7f3273c607e0615115f11
심현보 <hbsim0605@gmail.com>
2025-03-23 18:35:53 +0900

    replace clips

--- pages/mainPages/content/clipsContent.js
+++ pages/mainPages/content/clipsContent.js
@@ -24,25 +24,25 @@ export default function ClipsContent() {
             <div className="w-full">
               <BrunnerVideo
                 url={"https://youtu.be/MddvuCH-XUU"}
-                title={"S&P500 INDEX FUND"}
+                title={"S&P500 INDEX FUND."}
                 width="800px" // 100%
                 height="450px" // 100%
                 className="mt-5"
               ></BrunnerVideo>
               <BrunnerVideo
                 url={
-                  "https://www.youtube.com/watch?v=5nRs1niZ9h4&list=PLqb1KKjbyLLjjsGoYF9KlogyNIF3KDo0Z"
+                  "https://youtu.be/yUYzp_wNpks?t=10"
                 }
-                title={`The Advisors Alliance / 大军师司马懿之 军师联盟 (2017) / 대군사사마의지군사연맹`}
+                title={`S&P500 Cycles last 96 years.`}
                 width="800px" // 100%
                 height="450px" // 100%
                 className="mt-5"
               ></BrunnerVideo>
               <BrunnerVideo
                 url={
-                  "https://www.youtube.com/watch?v=UOmp8Zbvs7k&list=PLqb1KKjbyLLiVTGRO7cB_3PsbmbBRAvCj"
+                  "https://youtu.be/EIPl5Ml6wSk?t=8"
                 }
-                title={`Growling Tiger, Roaring Dragon / 大军师司马懿之 虎啸龙吟 (2017) / 대군사사마의지호소용음`}
+                title={`The rule of 4%.`}
                 width="800px" // 100%
                 height="450px"
                 className="mt-5" // 100%
