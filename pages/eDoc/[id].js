7b696bb84f0fe7c66bdab4e0ed9b07bdba35441c
심현보 <hbsim0605@gmail.com>
2025-07-16 10:46:06 +0900

    전자문서를 사용자 메뉴에 붙이기 작업 완료

--- pages/eDoc/[id].js
+++ pages/eDoc/[id].js
@@ -79,13 +79,12 @@ export default function EDocViewerPage() {
   }
 
   return (
-    <Layout>
+     <Layout>
       <Head>
         <title>Stock Quotes and Investment Information - Brunner-Next</title>
         <meta name="description" content="Brunner-Next provides real-time stock quotes. Make effective investments with real-time stock charts, investment strategies, stock news, and stock analysis tools."></meta>
-        <meta name="keywords" content="Stock, Stock Quotes, Stock Analysis, US Stock Quotes, Stock Investment, Real-Time Stock Quotes, Stock Analysis Tools, 주식, 주식 시세, 주식 분석, 미국 주식 시세, 주식 투자, 실시간 주식 시세, 주식 종목 분석"></meta>
         <meta rel="icon" href="/brunnerLogo.png"></meta>
-        <meta name="google-adsense-account" content="ca-pub-3879149687745447"></meta>
+        <link></link>
       </Head>
       <BodySection>
         <DivContainer>
@@ -115,6 +114,7 @@ export default function EDocViewerPage() {
 
           </div>
         </DivContainer>
+
       </BodySection>
     </Layout>
   );
