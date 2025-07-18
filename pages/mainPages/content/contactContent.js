7ce04b5ea0990f6ca999c76c7e0b4b6d8232b1ca
심현보 <hbsim0605@gmail.com>
2025-07-18 20:40:56 +0900

    웹캠  삭제

--- pages/mainPages/content/contactContent.js
+++ pages/mainPages/content/contactContent.js
@@ -26,9 +26,9 @@ export default function ContactContent() {
             {<ContactContentAnimation width={300} height={300} />}
           </div>
         )}
-        <div className={`w-full flex justify-start mt-10`}>
+        {/* <div className={`w-full flex justify-start mt-10`}>
           <BrunnerWebcamStream title="Brunner admin studio" />
-        </div>        
+        </div>         */}
       </DivContainer>
     </>
   );
