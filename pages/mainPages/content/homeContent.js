55689ef07a57ce58d39bb0efe7c544e0625500a9
심현보 <hbsim0605@gmail.com>
2025-07-20 15:32:52 +0900

    1

--- pages/mainPages/content/homeContent.js
+++ pages/mainPages/content/homeContent.js
@@ -16,13 +16,13 @@ export default function HomeContent() {
       <DivContainer className={`flex-row`}>
         <div className={`w-full items-center justify-center text-left`}>
           <h2 className={`title-font sm:text-4xl text-3xl mb-10 font-medium text-green-900`}>
-            Noesis Pelagos
+            Pelagos Voyager
           </h2>
           <GoverningMessage governingMessage={`사람의 생각은 무한하고 시시각각 변하지만 기억은 영원하지 않습니다. 
           그래서 생각은 기록으로 남기고 보전해야 합니다. 
           무한한 당신의 생각을 디지털 기록으로 보관하세요.`} />
           
-          <div className={`flex flex-col my-10`}>
+          <div className={`flex flex-col mt-20`}>
             {!userInfo.isLogin() && (
               <div>
                 <button
