fc74b03bf6924ed7f09b64b397246cbc546adc4e
심현보 <hbsim0605@gmail.com>
2025-07-20 15:49:32 +0900

    Revert "레이아웃 정리 끝" ㅇㅇ This reverts commit 971250a694267079002f49fc20aa08c6f8122040.

--- components/divContainer.js
+++ components/divContainer.js
@@ -2,7 +2,11 @@
 
 const DivContainer = ({ children, className = "" }) => {
   return (
+<<<<<<< HEAD
     <div className={`w-full relative ${className}`}>
+=======
+    <div className={`w-full relative my-10 ${className}`}>
+>>>>>>> parent of 971250a (레이아웃 정리 끝)
       {children}
     </div>
   );
