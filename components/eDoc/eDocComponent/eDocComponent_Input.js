87f9e14107ca7fb3c7eb9329dc7cb0b90327bd30
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-07-30 00:37:00 +0000

    값변경안됨수정

--- components/eDoc/eDocComponent/eDocComponent_Input.js
+++ components/eDoc/eDocComponent/eDocComponent_Input.js
@@ -120,7 +120,7 @@ export default function RenderComponent (props) {
       value={component.runtime_data?.value || ''}
       placeholder={component.runtime_data?.placeholder || ''}
       onClick={handleComponentClick}
-      onChange={(e) => onRuntimeDataChange( "value", e.target.value)}
+      onChange={(e) => onRuntimeDataChange({ runtime_data: { value: e.target.value } })}
     />
   );
 };
