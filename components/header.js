5035b7a12ca799e6310250832e0129c999c04f00
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-08-02 15:47:47 +0900

    로고도 왼쪽으로 이동

--- components/header.js
+++ components/header.js
@@ -10,31 +10,32 @@ export default function Header({ triggerMenuReload, reloadSignal }) {
   const UserInfo = userInfo.default;
 
   return (
-    <header className="sticky top-0 left-0 right-0 w-full z-50 bg-white dark:bg-slate-800 text-gray-600 dark:text-white body-font relative">
-  {/* 햄버거 메뉴 - 항상 우측 최상단 */}
-  <div className="absolute top-4 right-4 z-50">
-    <DropdownMenu triggerMenuReload={triggerMenuReload} reloadSignal={reloadSignal} />
-  </div>
+  <header className="sticky top-0 left-0 right-0 w-full z-50 bg-white dark:bg-slate-800 text-gray-600 dark:text-white body-font relative">
+  {/* 햄버거 메뉴 */}
+    <div className="absolute top-4 right-4 z-50">
+      <DropdownMenu triggerMenuReload={triggerMenuReload} reloadSignal={reloadSignal} />
+    </div>
 
-  <DivContainer className="flex items-center w-full md:w-2/3 mx-auto">
-    {/* 로고 */}
-    <Link
-      href="/"
-      className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-    >
-      <Image
-        src="/brunnerLogo202507.png"
-        alt="brunner logo"
-        priority
-        width={0}
-        height={0}
-        sizes="(max-width: 768px) 150px, 240px"
-        className="w-[150px] sm:w-[240px] h-auto"
-      />
-    </Link>
+    {/* 전체 너비로 확장된 컨테이너 */}
+    <DivContainer className="flex items-center w-full px-4">
+      {/* 로고 */}
+      <Link
+        href="/"
+        className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+      >
+        <Image
+          src="/brunnerLogo202507.png"
+          alt="brunner logo"
+          priority
+          width={0}
+          height={0}
+          sizes="(max-width: 768px) 150px, 240px"
+          className="w-[150px] sm:w-[240px] h-auto"
+        />
+      </Link>
 
-    <div className="flex-1" />
-  </DivContainer>
-</header>
+      <div className="flex-1" />
+    </DivContainer>
+  </header>
   );
 }
