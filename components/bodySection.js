a084325d326ecc59cd86609c5b038366d1d1f0b6
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-08-02 22:53:57 +0900

    바디 간격 2

--- components/bodySection.js
+++ components/bodySection.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 export default function BodySection({ children, triggerMenuReload }) {
   // children에 triggerMenuReload를 내려주기 (필요하다면)
   return (
-    <section className="my-2">
+    <section className="m-2">
       {React.Children.map(children, child =>
         React.isValidElement(child)
           ? React.cloneElement(child, { triggerMenuReload })
