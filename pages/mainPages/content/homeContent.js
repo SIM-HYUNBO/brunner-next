255228c1ace388d74c435633a7c490efa2054d5d
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-08-02 21:43:54 +0900

    푸터 확인

--- pages/mainPages/content/homeContent.js
+++ pages/mainPages/content/homeContent.js
@@ -15,36 +15,33 @@ export default function HomeContent() {
   return (
     <>
       <DivContainer className={`flex-row`}>
-        <div className={`w-full items-center justify-center text-left`}>
-          <h2 className={`page-title`}>
-            Noesis Pelagos
-          </h2>
-          <GoverningMessage governingMessage={`생각은 무한하고 시시각각 변하지만 기억은 영원하지 않습니다. 
-          그래서 생각은 기록으로 남기고 보전해야 합니다. 
-          무한한 당신의 생각을 디지털 기록으로 보관하세요.`} />
-          
-          <div className={`flex flex-col mt-20`}>
-            {!userInfo.isLogin() && (
-              <div>
-                <button
-                  className={`inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mr-2`}
-                  onClick={() => router.push("/mainPages/signin")}
-                >
-                  Sign in
-                </button>
-                <button
-                  className={`inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg`}
-                  onClick={() => router.push("/mainPages/signup")}
-                >
-                  Sign up
-                </button>
-              </div>
-            )}
-          </div>
-          <div className={`flex w-full mt-10 pr-5 readonly`}>
-            <BrunnerBoard boardType={'MAIN_TALK'} />
-          </div>
-
+        <h2 className={`page-title`}>
+          Noesis Pelagos
+        </h2>
+        <GoverningMessage governingMessage={`생각은 무한하고 시시각각 변하지만 기억은 영원하지 않습니다. 
+        그래서 생각은 기록으로 남기고 보전해야 합니다. 
+        무한한 당신의 생각을 디지털 기록으로 보관하세요.`} />
+        
+        <div className={`flex flex-col mt-20`}>
+          {!userInfo.isLogin() && (
+            <div>
+              <button
+                className={`inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mr-2`}
+                onClick={() => router.push("/mainPages/signin")}
+              >
+                Sign in
+              </button>
+              <button
+                className={`inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg`}
+                onClick={() => router.push("/mainPages/signup")}
+              >
+                Sign up
+              </button>
+            </div>
+          )}
+        </div>
+        <div className={`flex w-full mt-10 pr-5 readonly`}>
+          <BrunnerBoard boardType={'MAIN_TALK'} />
         </div>
         {!isMobile && (
           <div className={`items-center`}>
