00f9c7b7033d3702a8a5856256ed789b06946434
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-08-03 00:57:38 +0900

    디자이너 탑메뉴 버튼 텍스트

--- components/eDoc/eDocDesignerTopMenu.js
+++ components/eDoc/eDocDesignerTopMenu.js
@@ -17,46 +17,82 @@ export default function EDocDesignerTopMenu({
   onExportPdf
 }) {
   return (
-    <div className="w-full bg-slate-100 dark:bg-slate-800 py-2 flex items-center justify-center desktop:justify-left space-x-1 desktop:pr-80">
+    <div className="w-full 
+                    bg-slate-100 
+                    dark:bg-slate-800 
+                    flex 
+                    items-center 
+                    justify-center 
+                    space-x-1">
       <button
         onClick={onNewDocument}
-        className="px-3 py-1 bg-blue-400 text-white rounded hover:bg-blue-600"
+        className="bg-blue-400 
+                   text-white
+                   text-center 
+                   rounded 
+                   hover:bg-blue-600"
       >
         New
       </button>
       <button
         onClick={onOpenDocument}
-        className="px-3 py-1 bg-sky-400 text-white rounded hover:bg-sky-600"
+        className="bg-sky-400 
+                   text-white
+                   text-center 
+                   rounded 
+                   hover:bg-sky-600"
       >
         Open
       </button>
       <button
         onClick={onSaveDocument}
-        className="px-3 py-1 bg-yellow-400 text-white rounded hover:bg-yellow-600"
+        className="bg-yellow-400 
+                   text-white 
+                   text-center
+                   rounded 
+                   hover:bg-yellow-600"
       >
         Save
       </button>
       <button
         onClick={onDeleteDocument}
-        className="px-3 py-1 bg-red-400 text-white rounded ml-2 hover:bg-red-600"
+        className="bg-red-400 
+                   text-white 
+                   text-center
+                   rounded 
+                   ml-2 
+                   hover:bg-red-600"
       >
         Delete
       </button>
       <button 
         onClick={onAddPage}
-        className="px-3 py-1 bg-green-400 text-white rounded hover:bg-green-600"
+        className="bg-green-400 
+                   text-white 
+                   text-center
+                   rounded 
+                   hover:bg-green-600"
       >
         Add Page
       </button>
       <button
         onClick={onDeleteCurrentPage}
-        className="px-3 py-1 bg-pink-400 text-white rounded ml-2 hover:bg-pink-600"
+        className="bg-pink-400 
+                   text-white 
+                   text-center
+                   rounded 
+                   ml-2 
+                   hover:bg-pink-600"
       >
         Delete Page
       </button>
       <button
         onClick={onExportPdf}
-        className="px-3 py-1 bg-purple-400 text-white rounded hover:bg-purple-600"
+        className="bg-purple-400 
+                   text-white 
+                   text-center
+                   rounded 
+                   hover:bg-purple-600"
       >
         Export PDF
       </button>     
