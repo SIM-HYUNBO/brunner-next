46d7fb481a851ddd6614ad99654e29d32ea3d470
Hyunbo,Sim <39318738+SIM-HYUNBO@users.noreply.github.com>
2025-08-08 23:27:18 +0900

    로딩 컴포넌트 모듈화

--- components/brunnerMessageBox.js
+++ components/brunnerMessageBox.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import Loading from "@/components/Loading";
+import Loading from "@/components/loading";
 
 export const useModal = () => {
   const [modalContent, setModalContent] = useState({
