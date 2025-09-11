68aa9a08725be27b7d7eaa9b6b732ccb041e1800
심현보 <hbsim0605@gmail.com>
2025-09-11 16:23:04 +0900

    loading스피너 애니메이션 처리

--- components/lottiePlayer.js
+++ components/lottiePlayer.js
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 
 const Lottie = dynamic(()=> import("lottie-react"), { ssr : false});
 
-export default function LottiePlayer({ jsonString, width, height, loop = true, autoplay = true }) {
+export default function LottiePlayer({ jsonString, width, height, opacityValue = 1.0, loop = true, autoplay = true }) {
   // 문자열 → 객체 변환 (잘못된 JSON일 경우 대비)
   const animationData = useMemo(() => {
     try {
@@ -19,11 +19,13 @@ export default function LottiePlayer({ jsonString, width, height, loop = true, a
   }
 
   return (
+    <div style = {{opacity:opacityValue}}>
     <Lottie
       animationData={animationData}
       loop={loop}
       autoplay={autoplay}
       style={{ width, height }}
     />
+    </div>
   );
 }
