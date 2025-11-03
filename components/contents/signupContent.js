49a355709125b639be6cd14afefc2777e0051ff1
심현보 <hbsim0605@gmail.com>
2025-11-04 08:59:09 +0900

    선택시 주소 설정

--- components/contents/signupContent.js
+++ components/contents/signupContent.js
@@ -272,6 +272,7 @@ export default function SignupContent() {
               name="Address"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onChange={changeAddressValue}
+              value={address}
             />
           </div>
         </div>
