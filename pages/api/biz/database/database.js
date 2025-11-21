4bee2bd8062a604d31f6fbeee11bb84da7f4f80d
hbsim0605 <hbsim0605@gmail.com>
2025-11-22 02:17:03 +0900

    ssl true

--- pages/api/biz/database/database.js
+++ pages/api/biz/database/database.js
@@ -18,7 +18,7 @@ const getPool = async () => {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    port: process.env.DB_PORT,
     ssl: true,
   });
 };
